cb20ac1fefb934d7a9db8709cf3f364252ee2a58
Nickodee <nicodemusmuholo@gmail.com>
2024-05-20 12:03:22 +0300

    added responsiveness to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,25 +1,51 @@
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
+'use client'
+import Link from 'next/link';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { FaBarsStaggered } from 'react-icons/fa6';
+import { FaTimes } from "react-icons/fa";
+
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className='flex items-center p-4 bg-white w-full justify-between'>
-        <div>
-        <Image src='/logo.png' alt='logo' width={50} height={50} />
-        </div>
-        <ul className='flex gap-4 items-center'>
+    <>
+      <nav className='relative flex items-center p-4 bg-white w-full justify-between'>
+        <div className='flex gap-5 items-center'>
+          <Image src='/logo.png' alt='logo' width={50} height={50} />
+          <ul className='md:flex gap-4 items-center hidden md:block'>
             <li className='hover:text-[#1791C8]'><Link href='#'>Home</Link></li>
             <li className='hover:text-[#1791C8]'><Link href='#'>About</Link></li>
             <li className='hover:text-[#1791C8]'><Link href='#'>Services</Link></li>
             <li className='hover:text-[#1791C8]'><Link href='#'>Projects</Link></li>
             <li className='hover:text-[#1791C8]'><Link href='#'>Contacts</Link></li>
-        </ul>
-        <div>
-            <button className='rounded text-white p-2 hover:bg-[#0D7AB6] bg-[#1791C8]'>Get Started</button>
+          </ul>
         </div>
-    </nav>
-  )
+        <button className='rounded hidden md:block text-white p-2 hover:bg-[#0D7AB6] bg-[#1791C8]'>Get Started</button>
+        {menuOpen ? (
+          <FaTimes size={24} className='cursor-pointer md:hidden' onClick={toggleMenu} />
+        ) : (
+          <FaBarsStaggered size={24} className='cursor-pointer md:hidden' onClick={toggleMenu} />
+        )}
+      </nav>
+
+      {/* Sliding Menu */}
+      <div className={`fixed border-t-2 top-20 shadow-md right-0 w-full h-screen bg-white transition-transform transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden z-10`}>
+        <ul className='flex flex-col items-center h-full mt-3 gap-7'>
+          <li className='text-2xl hover:text-[#1791C8]'><Link href='#'>Home</Link></li>
+          <li className='text-2xl hover:text-[#1791C8]'><Link href='#'>About</Link></li>
+          <li className='text-2xl hover:text-[#1791C8]'><Link href='#'>Services</Link></li>
+          <li className='text-2xl hover:text-[#1791C8]'><Link href='#'>Projects</Link></li>
+          <li className='text-2xl hover:text-[#1791C8]'><Link href='#'>Contacts</Link></li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
